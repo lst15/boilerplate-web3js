16e48893a97a3f0607a2df2469536289552f5ab3
leandro <leandro.salustriano@hotmail.com>
2023-06-27 22:08:40 -0300

    fix code smell

--- src/modules/factories/ethers/ethers-pancake-swap-router-v1.factory.ts
+++ src/modules/factories/ethers/ethers-pancake-swap-router-v1.factory.ts
@@ -1,4 +1,3 @@
-import { EthersPancakeSwapFactoryV2ModuleRequest } from "../../repository/ethers/ethers-pancake-swap-factory-v2.module";
 import EthersPancakeSwapRouterV1Module, { EthersPancakeSwapRouterV1ModuleRequest } from "../../repository/ethers/ethers-pancake-swap-router-v1.module";
 
 class EthersPancakeSwapV1Factory {
