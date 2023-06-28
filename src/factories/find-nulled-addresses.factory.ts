a84eecc301100350d4f138b1d52d45cf41e89bd5
leandro <leandro.salustriano@hotmail.com>
2023-06-28 00:19:00 -0300

    added modular usecases

---
+++ src/factories/find-nulled-addresses.factory.ts
@@ -0,0 +1,15 @@
+import Endpoint from "../config/endpoint/endpoint";
+import EthersWebsocketProviderModule from "../modules/repository/ethers/ethers-websocket-provider.module";
+import { FindNulledAddressesUseCase } from "../usecases/find-nulled-addresses.usecase";
+
+export function FindNulledAddressesFactory(){
+  const endpointConfig = new Endpoint();
+  const endpoint = endpointConfig.config.list.chainstack.ws;
+
+  const websocketProviderModule = new EthersWebsocketProviderModule({
+    endpoint:endpoint
+  })  
+
+  const usecase = new FindNulledAddressesUseCase(websocketProviderModule)
+  return usecase;
+}
